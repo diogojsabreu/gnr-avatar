5350d8dcb354f35c0f98bd97326ca2535e40edc9
Diogo Abreu <diogojsabreu@gmail.com>
2024-03-27 11:22:33 +0000

    Update code and dependencies

--- interactive/api/detectLanguage/__init__.py
+++ interactive/api/detectLanguage/__init__.py
@@ -41,7 +41,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         "it": "it-IT",
         "ja": "ja-JP",
         "ko": "ko-KR",
-        "pt": "pt-PT",
+        "pt": "pt-BR",
         "zh_chs": "zh-CN",
         "zh_cht": "zh-CN",
         "ar": "ar-AE"
--- interactive/api/message/__init__.py
+++ interactive/api/message/__init__.py
@@ -4,38 +4,35 @@ import json
 import requests
 from datetime import datetime, timedelta
 import pyodbc
- 
+
 import azure.functions as func
- 
-logging.basicConfig(level=logging.DEBUG)
- 
- 
+
 search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
-search_key = os.getenv("AZURE_SEARCH_API_KEY")
+search_key = os.getenv("AZURE_SEARCH_API_KEY") 
 search_api_version = '2023-07-01-Preview'
-search_index_name = "indexboi"
- 
+search_index_name = os.getenv("AZURE_SEARCH_INDEX")
+
 AOAI_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 AOAI_key = os.getenv("AZURE_OPENAI_API_KEY")
 AOAI_api_version = os.getenv("AZURE_OPENAI_API_VERSION")
 embeddings_deployment = os.getenv("AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT")
 chat_deployment = os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT")
- 
+
 sql_db_server = os.getenv("SQL_DB_SERVER")
 sql_db_user = os.getenv("SQL_DB_USER")
 sql_db_password = os.getenv("SQL_DB_PASSWORD")
 sql_db_name = os.getenv("SQL_DB_NAME")
- 
+
 blob_sas_url = os.getenv("BLOB_SAS_URL")
- 
+
 server_connection_string = f"Driver={{ODBC Driver 17 for SQL Server}};Server=tcp:{sql_db_server},1433;Uid={sql_db_user};Pwd={sql_db_password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 database_connection_string = server_connection_string + f"Database={sql_db_name};"
- 
+
 # font color adjustments
 blue, end_blue = '\033[36m', '\033[0m'
- 
+
 place_orders = False
- 
+
 functions = [
     {
         "name": "get_bonus_points",
@@ -87,7 +84,7 @@ functions = [
             "required": ["account_id", "product_name", "quantity"],
         }
     },
-    {
+        {
         "name": "get_product_information",
         "description": "Find information about a product based on a user question. Use only if the requested information if not already available in the conversation context.",
         "parameters": {
@@ -100,92 +97,40 @@ functions = [
             },
             "required": ["user_question"],
         }
-    },
-    {
-        "name": "get_boi_information",
-        "description": "Find information about BOI based on a user question. Use only if the requested information if not already available in the conversation context.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "user_question": {
-                    "type": "string",
-                    "description": "User question (i.e., how much can i borrow?, etc.)"
-                },
-            },
-            "required": ["user_question"],
-        }
     }
 ]
- 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
- 
+
     messages = json.loads(req.get_body())
- 
+
     response = chat_complete(messages, functions= functions, function_call= "auto")
- 
-    logging.info("the response is: %s", response)
- 
+
     products = []
-   
+    
     try:
-        response_message = response["choices"][0]["message"]    
+        response_message = response["choices"][0]["message"]
     except:
         logging.info(response)
-       
+
     # if the model wants to call a function
     if response_message.get("function_call"):
-        logging.info("Response_message: %s", response_message)
         # Call the function. The JSON response may not always be valid so make sure to handle errors
-        if "name" not in response_message["function_call"]:
-            logging.error("Function name not found in response message.")
-            return func.HttpResponse("Function name not found in response message.", status_code=500)
- 
-        # Extract the function name from the function call information
-        function_call_info = response_message["function_call"]
-        logging.info("Function call info: %s", function_call_info)
- 
-        if "name" not in function_call_info:
-            logging.error("Function name not found in response message.")
-            return func.HttpResponse("Function name not found in response message.", status_code=500)
- 
-        function_name = function_call_info.get("name").strip()
-        logging.info("Extracted function name: %s", function_name)
- 
- 
-        function_arguments = function_call_info.get("arguments", {})  # default to empty dict if not provided
-        logging.info("FUNCTION NAME: %s", function_name)
-        logging.info("FUNCTION ARGUMENTS: %s", function_arguments)
-        # Log the response_message
-        logging.info("Response message: %s", response_message)
- 
-       
+        function_name = response_message["function_call"]["name"]
+
         available_functions = {
-            "get_bonus_points": get_bonus_points,
-            "get_order_details": get_order_details,
-            "order_product": order_product,
-            "get_product_information": get_product_information,
-            "get_boi_information": get_boi_information,
+                "get_bonus_points": get_bonus_points,
+                "get_order_details": get_order_details,
+                "order_product": order_product,
+                "get_product_information": get_product_information,
         }
- 
- 
-        # Check if the function name exists in available_functions dictionary
-        if function_name not in available_functions:
-            logging.error("Function '%s' not found.", function_name)
-            return func.HttpResponse(f"Function '{function_name}' not found.", status_code=500)
- 
-        # Retrieve the function from available_functions dictionary
-        function_to_call = available_functions[function_name]
-        logging.info("Function to call: %s", function_to_call)
- 
-        # Load function arguments from response_message["function_call"]["arguments"]
+        function_to_call = available_functions[function_name] 
+
         function_args = json.loads(response_message["function_call"]["arguments"])
- 
-        # Call the function with the loaded arguments
         function_response = function_to_call(**function_args)
-        #print(function_name, function_args)
-        logging.info("Function response: %s",function_response)
- 
+        # print(function_name, function_args)
+
         # Add the assistant response and function response to the messages
         messages.append({
             "role": response_message["role"],
@@ -195,23 +140,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             },
             "content": None
         })
- 
+
         if function_to_call == get_product_information:
             product_info = json.loads(function_response)
             # show product information after search for a different product that the current one
             # if product_info['product_image_file'] != current_product_image:
-               
+                
             products = [display_product_info(product_info)]
             current_product_image = product_info['product_image_file']
-           
-            # return only product description to LLM to avoid chatting about prices and image files
+            
+            # return only product description to LLM to avoid chatting about prices and image files 
             function_response = product_info['description']
-       
-        elif function_to_call == get_boi_information:
-            boi_info = json.loads(function_response)
-            # Process BOI information and update function_response
-            function_response = boi_info['content']
- 
+
         messages.append({
             "role": "function",
             "name": function_name,
@@ -219,73 +159,73 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         })
      
         response = chat_complete(messages, functions= functions, function_call= "none")
-       
+        
         response_message = response["choices"][0]["message"]
- 
+
     messages.append({'role' : response_message['role'], 'content' : response_message['content']})
- 
+
     logging.info(json.dumps(response_message))
- 
+
     response_object = {
         "messages": messages,
         "products": products
     }
- 
+
     return func.HttpResponse(
         json.dumps(response_object),
         status_code=200
     )
- 
+
 def execute_sql_query(query, connection_string=database_connection_string, params=None):
     """Execute a SQL query and return the results."""
     results = []
     print('database_connection_string', database_connection_string)
-   
+    
     # Establish the connection
     with pyodbc.connect(connection_string) as conn:
         cursor = conn.cursor()
-       
+        
         if params:
             cursor.execute(query, params)
         else:
             cursor.execute(query)
-       
+        
         # If the query is a SELECT statement, fetch results
         if query.strip().upper().startswith('SELECT'):
             results = cursor.fetchall()
-       
+        
         conn.commit()
- 
+
     return results
- 
+
 def get_bonus_points(account_id):
     """Retrieve bonus points and its cash value for a given account ID."""
      
     # Define the SQL query to retrieve loyalty_points for the given account_id
     query = "SELECT loyalty_points FROM Customers WHERE account_id = ?"
- 
+
     # Execute the query with account_id as a parameter
     results = execute_sql_query(query, params=(account_id,))
- 
+
     # If results are empty, return an error message in JSON format
     if not results:
         return json.dumps({"error": "Account not found"})
- 
+
     # Get the loyalty_points value
     loyalty_points = results[0][0]
- 
+
     # Convert loyalty_points to cash_value
     cash_value = loyalty_points / 9.5
- 
+
     # Create a JSON object with the required keys and values
     response_json = json.dumps({
         "available_bonus_points": loyalty_points,
         "cash_value": cash_value
     })
- 
+
     return response_json
- 
- 
+
+
 def get_order_details(account_id):
      
     # Get orders and corresponding product names for the account_id
@@ -296,10 +236,10 @@ def get_order_details(account_id):
         WHERE o.account_id = ?
     '''
     orders = execute_sql_query(query, params=(account_id,))
-   
+    
     # Get today's date and calculate the expected delivery date for each order
     today = datetime.today()
-   
+    
     # Create a JSON object with the required details
     order_details = [
         {
@@ -308,38 +248,38 @@ def get_order_details(account_id):
         }
         for order in orders
     ]
-   
+    
     # Return the JSON object
     return json.dumps(order_details)
- 
+
 def order_product(account_id, product_name, quantity=1):
      
     # Step 1: Find the maximum existing order_id
     query = "SELECT MAX(order_id) FROM Orders"
     results = execute_sql_query(query)
     max_order_id = results[0][0] if results[0][0] is not None else 0
- 
+
     # Step 2 & 3: Find product ID and check stock
     query = "SELECT id, name, stock FROM Products WHERE LOWER(name) LIKE LOWER(?)"
     params = (f'%{product_name}%',)
     results = execute_sql_query(query, params=params)
-   
+    
     # Handling no match found
     if not results:
         return json.dumps({"info": "No matching product found"})
-   
+    
     product_id, product_name_corrected, stock = results[0]
-   
+    
     # Check if the stock is sufficient
     if stock < quantity:
         return json.dumps({"info": "Insufficient stock"})
-   
+    
     # Step 4: Place the order
     # Deducting the ordered quantity from the stock
     query = "UPDATE Products SET stock = stock - ? WHERE id = ?"
     params = (quantity, product_id)
     if place_orders: execute_sql_query(query, params=params)
- 
+
     # Adding the order details to the Orders table
     days_to_delivery = 5
     for i in range(quantity):
@@ -347,71 +287,71 @@ def order_product(account_id, product_name, quantity=1):
         query = "INSERT INTO Orders (order_id, product_id, days_to_delivery, account_id) VALUES (?, ?, ?, ?)"
         params = (max_order_id, product_id, days_to_delivery, account_id)
         if place_orders: execute_sql_query(query, params=params)
-   
+    
     # Step 5: Calculate the expected delivery date and return the JSON object
     today = datetime.now()
     expected_delivery_date = today + timedelta(days=days_to_delivery)
-   
+    
     return json.dumps({
         "info": "Order placed",
         "product_name": product_name_corrected,
         "expected_delivery_date": expected_delivery_date.strftime('%Y-%m-%d')
     })
- 
+
 def display_product_info(product_info, display_size=40):
     """ Display product information """
- 
+
     # Show image
     image_file = product_info['product_image_file']
- 
+
     image_url = blob_sas_url.split("?")[0] + f"/{image_file}?" + blob_sas_url.split("?")[1]
- 
+
     response = requests.get(image_url)
     print(image_url)
- 
+
     # Check if the request was successful
     if response.status_code == 200:
         return {
             "tagline": product_info['tagline'],
             "original_price": product_info['original_price'],
             "special_offer": product_info['special_offer'],
-            "image_url": image_url
+            "image_url": image_url 
             }
     else:
         print(f"Failed to retrieve image. HTTP Status code: {response.status_code}")
- 
+
     print(f"""
     {product_info['tagline']}
-    Original price: ${product_info['original_price']} Special offer: ${product_info['special_offer']}
+    Original price: ${product_info['original_price']} Special offer: ${product_info['special_offer']} 
     """)
-   
+    
 def generate_embeddings(text):
     """ Generate embeddings for an input string using embeddings API """
- 
+
     url = f"{AOAI_endpoint}/openai/deployments/{embeddings_deployment}/embeddings?api-version={AOAI_api_version}"
- 
+
     headers = {
         "Content-Type": "application/json",
         "api-key": AOAI_key,
     }
- 
+
     data = {"input": text}
- 
+
     response = requests.post(url, headers=headers, data=json.dumps(data)).json()
     return response['data'][0]['embedding']
- 
+
 def get_product_information(user_question, categories='*', top_k=1):
     """ Vectorize user query to search Cognitive Search vector search on index_name. Optional filter on categories field. """
      
-    url = f"{search_endpoint}/indexes/indexboi/docs/search?api-version={search_api_version}"
- 
+    url = f"{search_endpoint}/indexes/{search_index_name}/docs/search?api-version={search_api_version}"
+
     headers = {
         "Content-Type": "application/json",
         "api-key": f"{search_key}",
     }
-   
+    
     vector = generate_embeddings(user_question)
- 
+
     data = {
         "vectors": [
             {
@@ -422,17 +362,17 @@ def get_product_information(user_question, categories='*', top_k=1):
         ],
         "select": "tagline, description, original_price, special_offer, product_image_file",
     }
- 
+
     # optional filtered search
     if categories != '*':
         data["filter"] = f"category eq '{categories}'"
- 
+
     results = requests.post(url, headers=headers, data=json.dumps(data))    
     results_json = results.json()
-   
+    
     # Extracting the required fields from the results JSON
     product_data = results_json['value'][0] # hard limit to top result for now
- 
+
     response_data = {
         "tagline": product_data.get('tagline'),
         "description": product_data.get('description'),
@@ -441,66 +381,24 @@ def get_product_information(user_question, categories='*', top_k=1):
         "product_image_file": product_data.get('product_image_file'),
     }
     return json.dumps(response_data)
- 
-############ BOI #############
-def get_boi_information(user_question, top_k=1):
-    """ Vectorize user query to search Cognitive Search vector search on index_name. Optional filter on categories field. """
-    #logging.info('Helloooooo from get_boi_information function!')
-    url = f"{search_endpoint}/indexes/{search_index_name}/docs/search?api-version={search_api_version}"
- 
-    headers = {
-        "Content-Type": "application/json",
-        "api-key": f"{search_key}",
-    }
-   
-    vector = generate_embeddings(user_question)
- 
-    data = {
-        "vectors": [
-            {
-                "value": vector,
-                "fields": "contentVector",
-                "k": top_k
-            },
-        ],
-        "select": "title, content, url",
-    }
- 
-    results = requests.post(url, headers=headers, data=json.dumps(data))    
-    results_json = results.json()
-   
-    # Extracting the required fields from the results JSON
-    boi_data = results_json['value'][0] # hard limit to top result for now
- 
-    response_data = {
-        "title": boi_data.get('title'),
-        "content": boi_data.get('content'),
-        "url": boi_data.get('url'),
-    }
-    return json.dumps(response_data)
-########## BOI #########################
- 
- 
+
 def chat_complete(messages, functions, function_call='auto'):
     """  Return assistant chat response based on user query. Assumes existing list of messages """
-   
+    
     url = f"{AOAI_endpoint}/openai/deployments/{chat_deployment}/chat/completions?api-version={AOAI_api_version}"
- 
+
     headers = {
         "Content-Type": "application/json",
         "api-key": AOAI_key
     }
- 
+
     data = {
         "messages": messages,
         "functions": functions,
         "function_call": function_call,
         "temperature" : 0,
     }
- 
-    logging.info("THE DATA IS: %s", data)
- 
-    response = requests.post(url, headers=headers, data=json.dumps(data)).json()
- 
-    return response
 
+    response = requests.post(url, headers=headers, data=json.dumps(data)).json()
+
+    return response
